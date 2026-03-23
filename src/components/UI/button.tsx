8012b61c63e10f898f192e562e836fb52f00afe3
Mitya <mitekk@gmail.com>
2026-03-24 00:25:28 +0200

    fix: center button text and make size responsive

--- src/components/UI/button.tsx
+++ src/components/UI/button.tsx
@@ -8,7 +8,7 @@ export const Button: React.FC<ButtonProps> = ({ children, onClick, style }) => (
   <div className="flex flex-col gap-2 items-center">
     <button
       style={style}
-      className={`bg-stone-900 px-6 py-2 rounded-lg text-white shadow min-h-20 text-2xl font-semibold`}
+      className={`bg-stone-900 px-6 py-2 rounded-lg text-white shadow min-h-12 md:min-h-16 lg:min-h-20 text-base md:text-xl lg:text-2xl font-semibold flex items-center justify-center`}
       onClick={onClick}
     >
       {children}
