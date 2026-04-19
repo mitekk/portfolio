64052999e47fe039b911b184107e70f3ca0011df
Mitya <mitekk@gmail.com>
2026-04-20 01:13:19 +0300

    feat: implement RouteTracker for page view tracking and enhance localStorage mock for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { MainLayout } from "./layout/main.layout";
 import { AppRoutes } from "./routes";
+import { trackPageView } from "./services/analytics/initAnalytics";
 import "./App.css";
+
+function RouteTracker() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    trackPageView(pathname);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
   return (
     <MainLayout>
       <BrowserRouter>
+        <RouteTracker />
         <AppRoutes />
       </BrowserRouter>
     </MainLayout>
